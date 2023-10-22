ba1cfff6c2a2ca847d3f5565bd25acd9e35985d1
Dominik Fryč <fryc.dominik@gmail.com>
2023-10-22 17:00:31 +0200

    feat: add element entry point

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,29 @@
 import { defineConfig } from "vite";
-import { fileURLToPath, URL } from "url";
+import { extname, relative } from "node:path";
+import { fileURLToPath } from "node:url";
+import { glob } from "glob";
 
 export default defineConfig({
   build: {
-    sourcemap: true,
     lib: {
-      entry: fileURLToPath(new URL("src/index.ts", import.meta.url)),
-      name: "Diffix",
-      fileName: "index",
+      entry: Object.fromEntries(
+        glob
+          .sync("src/**/*.ts")
+          .map((file) => [
+            relative("src", file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
     },
+    rollupOptions: {
+      output: [
+        {
+          assetFileNames: "assets/[name][extname]",
+          entryFileNames: "[name].js",
+        },
+      ],
+    },
+    target: "esnext",
+    sourcemap: true,
   },
 });
